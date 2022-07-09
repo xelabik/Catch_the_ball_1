f16307216f87514d337690a69e2e6184752474e1
xelabik <nidrabikxela@gmail.com>
2022-07-09 15:53:41 +0300

    Add space

--- test.py
+++ test.py
@@ -2,9 +2,9 @@
 
 
 def print_hi(name:str)-> None:
-    print(f"Hi, {name}") 
+    print(f"Hi  {name}")
 
 if __name__ == "__main__":
     friends: list = ["Dima", "Demon"]
-    print_hi(name=",".join(friends))
+    print_hi(name=", ".join(friends))
 
